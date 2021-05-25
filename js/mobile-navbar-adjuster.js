f522b57100af944c375cf328676e91e7b80e3762
techx-guy <70029654+techx-guy@users.noreply.github.com>
2021-05-25 16:35:38 -0400

    update

--- js/mobile-navbar-adjuster.js
+++ js/mobile-navbar-adjuster.js
@@ -1,6 +1,6 @@
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-
+var navbarToggler = document.getElementById('navbarNavDropdown')
 $(document).ready(function() {
     // executes when HTML-Document is loaded and DOM is ready
     //If refreshing page, check current location to prevent unwanted transparent navbar
@@ -19,7 +19,7 @@ $(document).ready(function() {
         $(".navbar").removeClass("mobile-navbar-collapsed");
       }
 
-      else if (("navbarNavDropdown").hasClass("show"))
+      else if (navbarToggler.hasClass("show"))
       {
         $(".navbar").add("mobile-navbar-dropdown");
         $(".navbar").removeClass("mobile-navbar-collapsed");
