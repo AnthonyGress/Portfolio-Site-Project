6b50f4ca535598d368d694cc376a88b4f8ec1f90
techx-guy <70029654+techx-guy@users.noreply.github.com>
2021-05-25 16:29:24 -0400

    updates

--- js/autohide-clean.js
+++ js/autohide-clean.js
@@ -8,18 +8,18 @@ window.onscroll = function() {
     document.getElementById("navbar").classList.remove('scrolled-down');
     document.getElementById("navbar").classList.add('navbar-blur-dark');
     document.getElementById("navbar").classList.remove('navbar-blur');
+    if (currentScrollPos <= 0) {
+      document.getElementById("navbar").classList.add('scrolled-up');
+      document.getElementById("navbar").classList.remove('scrolled-down');
+      document.getElementById("navbar").classList.add('navbar-blur');
+      document.getElementById("navbar").classList.remove('navbar-blur-dark');
+    }
     if (_mobileMenuOpen === true) {
         $("button.navbar-toggler").click();
     }
   }
-  else if (currentScrollPos<=0) {
-    document.getElementById("navbar").classList.add('scrolled-up');
-    document.getElementById("navbar").classList.remove('scrolled-down');
-    document.getElementById("navbar").classList.add('navbar-blur');
-    document.getElementById("navbar").classList.remove('navbar-blur-dark');
-    
-  }
-  else {
+
+  else if (prevScrollpos < currentScrollPos){
     document.getElementById("navbar").classList.remove('scrolled-up');
     document.getElementById("navbar").classList.add('scrolled-down');
   }
---
+++ js/mobile-navbar-adjuster.js
@@ -0,0 +1,34 @@
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+    //If refreshing page, check current location to prevent unwanted transparent navbar
+    /*
+    ################
+    Add navbar background color when scrolled
+    */
+    
+      if (vw < 750) {
+        
+        $(".navbar").removeClass("mobile-navbar-dropdown");
+        $(".navbar").addClass("mobile-navbar-collapsed");
+      } 
+      else if (vw > 750){
+        $(".navbar").removeClass("mobile-navbar-dropdown");
+        $(".navbar").removeClass("mobile-navbar-collapsed");
+      }
+
+      else if (document.getElementById('navbar-toggler').onclick==true)
+      {
+        alert("clicked");
+      }
+        
+    
+    });
+
+
+    // ############
+  
+    // document ready
+
